3170c43fe729f656f5b13f5e27d1470113d444ec
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-01-24 17:15:50 +0000

    fix quotes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
-#include <iostream>
-
-int main() {
-  std::cout << "Hello World!\n";
+#include<iostream> 
+using namespace std;
+int main()
+{
+	int day; 
+  cin>>day; 
+	
+  switch(day) {
+		     case 1 : cout << "today is Monday"; break;
+		     case 2 : cout << "today is Tuesday"; break;
+		     case 3 : cout << "today is Wednesday"; break;
+		     case 4 : cout << "today is Thursday"; break;
+		     case 5 : cout << "today is Friday"; break;
+		     case 6 : cout << "today is Saturday"; break;
+		     case 7 : cout << "today is Sunday"; break;
+				 default: cout << "Invalid number";
+	}
+	return 0; 
 }
